732830975f5998b624df38cface1fb28e4d33ae4
greesh13 <greeshmashekhar.y@gmail.com>
2024-07-07 22:11:30 -0400

    run_java_gatway.sh is changed

--- mypackage/run_java_gateway.sh
+++ mypackage/run_java_gateway.sh
@@ -6,5 +6,5 @@ PY4J_PATH="$(dirname $0)/py4j-0.10.9.5.jar"
 # Compile the Java file
 javac -cp $PY4J_PATH -d $(dirname $0) $(dirname $0)/MathOperations.java
 
-# Run the Java Gateway file
+# Run the Java Gateway file in the directory
 java -cp "$PY4J_PATH:$(dirname $0)" mypackage.MathOperations > $(dirname $0)/gateway.log 2>&1 &
