bf13ec4ad63de8e16e5916923b4ee7be636d3dc4
greesh13 <greeshmashekhar.y@gmail.com>
2024-07-07 22:00:04 -0400

    Test cases for Arithmetic Operations

--- mypackage/run_java_gateway.sh
+++ mypackage/run_java_gateway.sh
@@ -6,5 +6,5 @@ PY4J_PATH="$(dirname $0)/py4j-0.10.9.5.jar"
 # Compile the Java file
 javac -cp $PY4J_PATH -d $(dirname $0) $(dirname $0)/MathOperations.java
 
-# Run the Java Gateway
+# Run the Java Gateway file
 java -cp "$PY4J_PATH:$(dirname $0)" mypackage.MathOperations > $(dirname $0)/gateway.log 2>&1 &
